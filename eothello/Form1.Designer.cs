b379dd2b74de0a53696311423b28f8929928d9d7
mitch <mitch@DESKTOP-E3GNECJ>
2023-11-01 02:39:30 +0000

    AMMMZIING

--- eothello/Form1.Designer.cs
+++ eothello/Form1.Designer.cs
@@ -28,18 +28,185 @@
         /// </summary>
         private void InitializeComponent()
         {
-            SuspendLayout();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.gameToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.settingsToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.pictureBox3 = new System.Windows.Forms.PictureBox();
+            this.pictureBox4 = new System.Windows.Forms.PictureBox();
+            this.pictureBox5 = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.menuStrip1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox5)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.gameToolStripMenuItem,
+            this.settingsToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(784, 24);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // gameToolStripMenuItem
+            // 
+            this.gameToolStripMenuItem.Name = "gameToolStripMenuItem";
+            this.gameToolStripMenuItem.Size = new System.Drawing.Size(50, 20);
+            this.gameToolStripMenuItem.Text = "Game";
+            // 
+            // settingsToolStripMenuItem
+            // 
+            this.settingsToolStripMenuItem.Name = "settingsToolStripMenuItem";
+            this.settingsToolStripMenuItem.Size = new System.Drawing.Size(61, 20);
+            this.settingsToolStripMenuItem.Text = "Settings";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.Color.Green;
+            this.pictureBox1.Location = new System.Drawing.Point(-186, 665);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(1337, 75);
+            this.pictureBox1.TabIndex = 1;
+            this.pictureBox1.TabStop = false;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.BackColor = System.Drawing.Color.White;
+            this.pictureBox2.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pictureBox2.Location = new System.Drawing.Point(506, 678);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Padding = new System.Windows.Forms.Padding(30);
+            this.pictureBox2.Size = new System.Drawing.Size(50, 50);
+            this.pictureBox2.TabIndex = 2;
+            this.pictureBox2.TabStop = false;
+            // 
+            // pictureBox3
+            // 
+            this.pictureBox3.BackColor = System.Drawing.Color.Black;
+            this.pictureBox3.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.pictureBox3.Location = new System.Drawing.Point(128, 678);
+            this.pictureBox3.Name = "pictureBox3";
+            this.pictureBox3.Size = new System.Drawing.Size(50, 50);
+            this.pictureBox3.TabIndex = 3;
+            this.pictureBox3.TabStop = false;
+            // 
+            // pictureBox4
+            // 
+            this.pictureBox4.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(255)))), ((int)(((byte)(128)))));
+            this.pictureBox4.Location = new System.Drawing.Point(489, 671);
+            this.pictureBox4.Name = "pictureBox4";
+            this.pictureBox4.Size = new System.Drawing.Size(271, 61);
+            this.pictureBox4.TabIndex = 4;
+            this.pictureBox4.TabStop = false;
+            // 
+            // pictureBox5
+            // 
+            this.pictureBox5.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(255)))), ((int)(((byte)(128)))));
+            this.pictureBox5.Location = new System.Drawing.Point(108, 671);
+            this.pictureBox5.Name = "pictureBox5";
+            this.pictureBox5.Size = new System.Drawing.Size(283, 61);
+            this.pictureBox5.TabIndex = 5;
+            this.pictureBox5.TabStop = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.Green;
+            this.label1.Font = new System.Drawing.Font("Segoe UI", 30F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label1.Location = new System.Drawing.Point(24, 674);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(45, 54);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "0";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.Color.Green;
+            this.label2.Font = new System.Drawing.Font("Segoe UI", 30F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label2.Location = new System.Drawing.Point(412, 674);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(45, 54);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "0";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.BackColor = System.Drawing.Color.Green;
+            this.label3.Font = new System.Drawing.Font("Rockwell", 25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label3.Location = new System.Drawing.Point(577, 685);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(166, 38);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "Player #2";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.BackColor = System.Drawing.Color.Green;
+            this.label4.Font = new System.Drawing.Font("Rockwell", 25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label4.Location = new System.Drawing.Point(197, 685);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(175, 38);
+            this.label4.TabIndex = 9;
+            this.label4.Text = "Player #1 ";
             // 
             // Form1
             // 
-            AutoScaleDimensions = new SizeF(7F, 15F);
-            AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(784, 761);
-            Name = "Form1";
-            Text = "Form1";
-            ResumeLayout(false);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(64)))), ((int)(((byte)(0)))));
+            this.ClientSize = new System.Drawing.Size(784, 761);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.pictureBox3);
+            this.Controls.Add(this.pictureBox5);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.pictureBox4);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox5)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private MenuStrip menuStrip1;
+        private ToolStripMenuItem gameToolStripMenuItem;
+        private ToolStripMenuItem settingsToolStripMenuItem;
+        private PictureBox pictureBox1;
+        private PictureBox pictureBox2;
+        private PictureBox pictureBox3;
+        private PictureBox pictureBox4;
+        private PictureBox pictureBox5;
+        private Label label1;
+        private Label label2;
+        private Label label3;
+        private Label label4;
     }
 }
--- eothello/Form1.cs
+++ eothello/Form1.cs
@@ -5,6 +5,8 @@ namespace eothello
 {
     public partial class Form1 : Form
     {
+
+        // Define Boardsize 
         const int NUM_OF_BOARD_ROWS = 8;
         const int NUM_OF_BOARD_COL = 8;
 
@@ -13,14 +15,15 @@ namespace eothello
         int[,] gameBoardData;
         string tileImagesDirPath = "Resources/";
         public int TurnCounter = 1;
-
+        public bool FirstMove = true;
         public Form1()
         {
             InitializeComponent();
+            Point top = new Point(30, 30);
+            Point bottom = new Point(120, 120);
+            
 
-            Point top = new Point(10, 10);
-            Point bottom = new Point(10, 10);
-
+            // updates the games state, actual tiles = (_gameboardgui) LoggedPositions = (gameboardData)
             gameBoardData = this.MakeBoardArray();
             
             try
@@ -29,7 +32,7 @@ namespace eothello
                 _gameBoardGui.TileClicked += new GameboardImageArray.TileClickedEventDelegate(GameTileClicked);
                 _gameBoardGui.UpdateBoardGui(gameBoardData);
                 IterateThroughBoard();
-
+                UserInterface();
             }
             catch (Exception ex)
             {
@@ -39,154 +42,7 @@ namespace eothello
 
         }
 
-
-        public bool isValidMove(int row, int col)
-        {
-            if (gameBoardData[row, col] == 0)
-            {
-                Console.WriteLine(TurnCounter.ToString()); // Print the current TurnCounter value.
-
-                Point ScanPos = new Point(row, col);
-                (int dx, int dy)[] offsets =
-                {
-            (1, 0), (1, 1), (0, 1), (-1, 1),
-            (-1, 0), (-1, -1), (0, -1), (1, -1)
-        };
-
-                foreach (var offset in offsets)
-                {
-                    List<Point> capturedPoints = new List<Point>();
-
-                    int newX = ScanPos.X + offset.dx;
-                    int newY = ScanPos.Y + offset.dy;
-
-                    if (IsWithinBoard(newX, newY) && gameBoardData[newX, newY] != TurnCounter)
-                    {
-                        int opponentColor = TurnCounter == 1 ? 10 : 1;
-
-                        while (IsWithinBoard(newX, newY) && gameBoardData[newX, newY] == opponentColor)
-                        {
-                            capturedPoints.Add(new Point(newX, newY));
-
-                            newX += offset.dx;
-                            newY += offset.dy;
-                        }
-
-                        if (IsWithinBoard(newX, newY) && gameBoardData[newX, newY] == TurnCounter && capturedPoints.Count > 0)
-                        {
-                            // A valid move is found
-                            return true; // Return true without making the move
-                        }
-                    }
-                }
-            }
-
-            return false; // Return false if no valid move is found at (row, col)
-        }
-
-        public void IterateThroughBoard()
-        {
-
-            for (int row = 0; row < NUM_OF_BOARD_ROWS; row++)
-            {
-                for (int col = 0; col < NUM_OF_BOARD_COL; col++)
-                {
-                    if (isValidMove(row, col))
-                    {
-                        _gameBoardGui.SetTile(row, col, 100.ToString());
-                        gameBoardData[row, col] = 100;
-                    }
-                }
-            }
-        }
-
-        public void Dispose1()
-        {
-
-            for (int row = 0; row < NUM_OF_BOARD_ROWS; row++)
-            {
-                for (int col = 0; col < NUM_OF_BOARD_COL; col++)
-                {
-                    if (gameBoardData[row, col] == 100)
-                    {
-                        _gameBoardGui.SetTile(row, col, 0.ToString());
-                    }
-                }
-            }
-        }
-
-
-
-
-
-
-        public void ValidMove(int row, int col)
-        {
-            if (gameBoardData[row, col] == 0 || gameBoardData[row, col] == 100)
-            {
-                Console.WriteLine(TurnCounter.ToString()); // Print the current TurnCounter value.
-      
-                Point ScanPos = new Point(row, col);
-                (int dx, int dy)[] offsets =
-                {
-            (1, 0), (1, 1), (0, 1), (-1, 1),
-            (-1, 0), (-1, -1), (0, -1), (1, -1)
-        };
-      
-                bool validMoveFound = false;
-      
-                foreach (var offset in offsets)
-                {
-                    List<Point> capturedPoints = new List<Point>();
-      
-                    int newX = ScanPos.X + offset.dx;
-                    int newY = ScanPos.Y + offset.dy;
-      
-                    if (IsWithinBoard(newX, newY) && gameBoardData[newX, newY] != TurnCounter)
-                    {
-                        int opponentColor = TurnCounter == 1 ? 10 : 1;
-      
-                        while (IsWithinBoard(newX, newY) && gameBoardData[newX, newY] == opponentColor)
-                        {
-                            capturedPoints.Add(new Point(newX, newY));
-      
-                            newX += offset.dx;
-                            newY += offset.dy;
-                        }
-      
-                        if (IsWithinBoard(newX, newY) && gameBoardData[newX, newY] == TurnCounter && capturedPoints.Count > 0)
-                        {
-                            // A valid move is found
-                            int color = TurnCounter;
-                            _gameBoardGui.SetTile(row, col, color.ToString());
-                            gameBoardData[row, col] = TurnCounter;
-      
-                            foreach (Point capturedPoint in capturedPoints)
-                            {
-                                gameBoardData[capturedPoint.X, capturedPoint.Y] = TurnCounter;
-                                _gameBoardGui.SetTile(capturedPoint.X, capturedPoint.Y, color.ToString());
-                            }
-      
-                            validMoveFound = true;
-                        }
-                    }
-                }
-                if (validMoveFound)
-                {
-                    // Toggle the TurnCounter for the next player's turn
-                    TurnCounter = (TurnCounter == 1) ? 10 : 1;
-                }
-            }
-        }
-      
-      private bool IsWithinBoard(int x, int y)
-      {
-         // Check if the coordinates (x, y) are within the boundaries of the game board.
-          return x >= 0 && x < gameBoardData.GetLength(0) && y >= 0 && y < gameBoardData.GetLength(1);
-      }
-
-
-
+        //When game is started it uses this to setup the board
         private int[,] MakeBoardArray()
         {
             int[,] BoardArray = new int[NUM_OF_BOARD_ROWS, NUM_OF_BOARD_COL];
@@ -199,18 +55,219 @@ namespace eothello
 
         }
 
+        // Tuple to return if a position ((0 - 0) --> (8, 8)) = is a validmove, and if it is returns correct tiles 
+        public (bool IsValid, List<Point> CapturedPoints) IsValidMove(int row, int col)
+        {
+            List<Point> capturedPoints = new List<Point>();
+
+            if (gameBoardData[row, col] == 0 || gameBoardData[row, col] == 100)
+            {
+                Console.WriteLine(TurnCounter.ToString()); // Print the current TurnCounter value
+
+                Point ScanPos = new Point(row, col);
+                (int dx, int dy)[] offsets =
+                {
+            (1, 0), (1, 1), (0, 1), (-1, 1),
+            (-1, 0), (-1, -1), (0, -1), (1, -1)
+        };
+
+                foreach (var offset in offsets)
+                {
+                    int newX = ScanPos.X + offset.dx;
+                    int newY = ScanPos.Y + offset.dy;
+
+                    if (IsWithinBoard(newX, newY) && gameBoardData[newX, newY] != TurnCounter)
+                    {
+                        int opponentColor = TurnCounter == 1 ? 10 : 1;
+                        List<Point> tempCapturedPoints = new List<Point>();
+
+                        while (IsWithinBoard(newX, newY) && gameBoardData[newX, newY] == opponentColor)
+                        {
+                            tempCapturedPoints.Add(new Point(newX, newY));
+                            newX += offset.dx;
+                            newY += offset.dy;
+                        }
+
+                        if (IsWithinBoard(newX, newY) && gameBoardData[newX, newY] == TurnCounter && tempCapturedPoints.Count > 0)
+                        {
+                            // A valid move is found
+                            capturedPoints.AddRange(tempCapturedPoints);
+                        }
+                    }
+                }
+
+                if (capturedPoints.Count > 0)
+                {
+                    return (true, capturedPoints); // Return true and the captured points
+                }
+            }
+
+            return (false, capturedPoints); // Return false and an empty list of captured points
+        }
+
+
+        // does what it says on tin ;)
+        private bool IsWithinBoard(int x, int y)
+      {     
+          return x >= 0 && x < gameBoardData.GetLength(0) && y >= 0 && y < gameBoardData.GetLength(1);
+      }
+
+
+        // Draws the valid squares with an outlined box
+        public void IterateThroughBoard()
+        {
+            for (int row = 0; row < NUM_OF_BOARD_ROWS; row++)
+            {
+                for (int col = 0; col < NUM_OF_BOARD_COL; col++)
+                {
+                    var (isValid, capturedPoints) = IsValidMove(row, col);
+
+                    if (isValid)
+                    {
+                        _gameBoardGui.SetTile(row, col, 100.ToString());
+                        gameBoardData[row, col] = 100;
+                    }
+                }
+            }
+  
+        }
+
+        // the turns should clear the board of any valid moves markers 
+        public void DisposeOfValidMoves()
+        {
+
+            for (int row = 0; row < NUM_OF_BOARD_ROWS; row++)
+            {
+                for (int col = 0; col < NUM_OF_BOARD_COL; col++)
+                {
+                    if (gameBoardData[row, col] == 100)
+                    {
+                        _gameBoardGui.SetTile(row, col, 0.ToString());
+                    }
+                }
+            }
+
+        }
+
+        public bool AnyValidMoveLeft(int player)
+        {
+            for (int row = 0; row < NUM_OF_BOARD_ROWS; row++)
+            {
+                for (int col = 0; col < NUM_OF_BOARD_COL; col++)
+                {
+                    var (isValid, _) = IsValidMove(row, col);
+                    if (isValid)
+                    {
+                        return true;
+                    }
+                }
+            }
+            return false;
+        }
+
+
+        public (int Player1Count, int Player10Count) CalculateWinner()
+        {
+            int player1Count = 0;
+            int player10Count = 0;
+
+            // Count the pieces for each player
+            for (int row = 0; row < NUM_OF_BOARD_ROWS; row++)
+            {
+                for (int col = 0; col < NUM_OF_BOARD_COL; col++)
+                {
+                    if (gameBoardData[row, col] == 1)
+                    {
+                        player1Count++;
+
+                    }
+                    else if (gameBoardData[row, col] == 10)
+                    {
+                        player10Count++;
+                    }
+                }
+            }
+            if (!AnyValidMoveLeft(1) && !AnyValidMoveLeft(10))
+            {
+                if (player1Count > player10Count)
+                {
+                    // Player 1 (color 1) wins
+                    MessageBox.Show("Player 1 Wins");
+                    
+                }
+                else if (player10Count > player1Count)
+                {
+                    // Player 2 (color 10) wins
+                    MessageBox.Show("Player 2 Wins");
+
+                }
+                else
+                {
+                    // It's a tie
+                    MessageBox.Show("It's a tie");
+
+                }
+                
+            }
+            return (player1Count, player10Count);
+        }
+
+
+        public void UserInterface()
+        {
+            var (player1Count, player10Count) = CalculateWinner();
+            label2.Text = player10Count.ToString();
+            label1.Text = player1Count.ToString();
+            Color color = Color.FromArgb(123, 255, 128);
+            if(TurnCounter == 10)
+            {
+                pictureBox4.BackColor = color;
+                pictureBox5.BackColor = Color.Green;
+            }
+            else
+            {
+                pictureBox5.BackColor = color;
+                pictureBox4.BackColor = Color.Green;
+            }
+  
+        }
+
+        // checks if move is valid using isvalidMove and then places it if it is
         public void GameTileClicked(object sender, EventArgs e)
         {
             int selectionRow = _gameBoardGui.GetCurrentRowIndex(sender);
             int selectionCol = _gameBoardGui.GetCurrentColumnIndex(sender);
-            Dispose1();
-            ValidMove(selectionRow, selectionCol);
-            IterateThroughBoard();
-           
 
+            var (isValid, capturedPoints) = IsValidMove(selectionRow, selectionCol);
+
+            if (isValid)
+            {
+                int color = TurnCounter;             
+                DisposeOfValidMoves();
+
+                _gameBoardGui.SetTile(selectionRow, selectionCol, color.ToString());
+                gameBoardData[selectionRow, selectionCol] = TurnCounter;
+
+              
+
+                // Set all captured points to the current player's color
+                foreach (Point capturedPoint in capturedPoints)
+                {
+                    gameBoardData[capturedPoint.X, capturedPoint.Y] = TurnCounter;
+                    _gameBoardGui.SetTile(capturedPoint.X, capturedPoint.Y, color.ToString());
+                }
+
+               
+                TurnCounter = (TurnCounter == 1) ? 10 : 1;
+
+                // Redisplay valid moves for the next player
+                IterateThroughBoard();
+
+                
+
+            }
+            UserInterface();
         }
-
-
     }
 
 }
